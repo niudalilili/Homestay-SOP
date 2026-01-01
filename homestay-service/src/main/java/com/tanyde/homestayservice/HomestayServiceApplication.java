2f710a7b4a7d64ba9f26bc6044ae8e4e6ffb8094
niudalilili <609924214@qq.com>
2026-01-01 19:19:46 +0800

    环境配置(遗漏的启动项上传）

---
+++ homestay-service/src/main/java/com/tanyde/homestayservice/HomestayServiceApplication.java
@@ -0,0 +1,16 @@
+package com.tanyde.homestayservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class HomestayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomestayServiceApplication.class, args);
+        log.info("sever started");
+    }
+
+}
